f28009bad9cb6020e0d7bec54132f42fc28d30f8
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2018-03-25 18:12:51 -0300

    Librerías locales y Cache-Control

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,36 +1,27 @@
-/**
- * Main JS file for Casper behaviours
- */
-
-/*globals jQuery, document */
 (function ($) {
     "use strict";
-
     $(document).ready(function(){
         $(".post-content").fitVids();
-
-        // Creates Captions from Alt tags
         $(".post-content img").each(function() {
-            // Let's put a caption if there is one
-            if($(this).attr("alt") && !$(this).hasClass("emoji"))
-              $(this).wrap('<figure class="image"></figure>')
-              .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
+            if ($(this).attr("alt") && !$(this).hasClass("emoji"))
+                $(this).wrap('<figure class="image"></figure>')
+                .after('<figcaption>'+$(this).attr("alt")+'</figcaption>');
         });
-
         $(window).scroll(function(){
-            if(($(window).scrollTop() / $(window).height()) > 1) {
-              $('.logo-readium').show();
-            } else {
-              $('.logo-readium').hide();
-            }
+            if(($(window).scrollTop() / $(window).height()) > 1)
+                $('.logo-readium').show();
+            else 
+                $('.logo-readium').hide();
         });
-
         $(".viewmore.head").click(function() {
-          $('html, body').animate({
-              scrollTop: $(window).height()
-          }, 400);
+          $('html, body').animate({ scrollTop: $(window).height() }, 400);
         });
-
+        $(window).on('scroll', function() {
+            var top = $(window).scrollTop();
+            if (top < 0 || top > 1500) return;
+            $('.post-image-image, .teaserimage-image').css('transform', 'translate3d(0px, '+top/3+'px, 0px)').css('opacity', 1-Math.max(top/700, 0));
+        });
+        $(window).trigger('scroll');
+        $('.post-content').css('padding-top', $('.article-image').height() + 'px');
     });
-
 }(jQuery));
