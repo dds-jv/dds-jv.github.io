e761acda5b67d494e59bd1ae9367ae4ad318c14d
Roberto Ruiz <rruiz@est.frba.utn.edu.ar>
2018-03-26 23:02:53 -0300

    Depurando gems antiguos

--- vendor/bundle/ruby/2.4.0/bin/kramdown.bat
+++ vendor/bundle/ruby/2.4.0/bin/kramdown.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 IF NOT "%~f0" == "~f0" GOTO :WinNT
-@"C:\Ruby24-x64\bin\ruby.exe" "D:/Repositorios/dds-alpha/vendor/bundle/ruby/2.4.0/bin/kramdown" %1 %2 %3 %4 %5 %6 %7 %8 %9
+@"C:\Ruby24-x64\bin\ruby.exe" "D:/Repositorios/dds-jv.github.io/vendor/bundle/ruby/2.4.0/bin/kramdown" %1 %2 %3 %4 %5 %6 %7 %8 %9
 GOTO :EOF
 :WinNT
 @"C:\Ruby24-x64\bin\ruby.exe" "%~dpn0" %*
